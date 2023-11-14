383aedab81fd47f4ba40f7605dd79fe3bc502a05
Julia Tam <142691507+juliartam@users.noreply.github.com>
2023-11-14 10:14:08 -0500

    #fixed user register errors

--- client/src/components/registration/RegistrationForm.js
+++ client/src/components/registration/RegistrationForm.js
@@ -1,40 +1,41 @@
 import React, { useState } from "react";
 import FormError from "../layout/FormError";
-import ErrorList from "../services/ErrorList"
-import translateServerErrors from "../../services/translateServerErrors"
+import ErrorList from "../layout/ErrorList";
+import translateServerErrors from "../../services/translateServerErrors";
 import config from "../../config";
-​
+
 const RegistrationForm = () => {
   const [userPayload, setUserPayload] = useState({
     email: "",
     password: "",
     passwordConfirmation: "",
   });
-​
+
   const [errors, setErrors] = useState({});
-​
+  const [serverErrors, setServerErrors] = useState({});
+
   const [shouldRedirect, setShouldRedirect] = useState(false);
-​
+
   const validateInput = (payload) => {
     setErrors({});
     const { email, password, passwordConfirmation } = payload;
     const emailRegexp = config.validation.email.regexp.emailRegex;
     let newErrors = {};
-​
+
     if (!email.match(emailRegexp)) {
       newErrors = {
         ...newErrors,
         email: "is invalid",
       };
     }
-​
+
     if (password.trim() == "") {
       newErrors = {
         ...newErrors,
         password: "is required",
       };
     }
-​
+
     if (passwordConfirmation.trim() === "") {
       newErrors = {
         ...newErrors,
@@ -48,14 +49,14 @@ const RegistrationForm = () => {
         };
       }
     }
-​
+
     setErrors(newErrors);
     if (Object.keys(newErrors).length === 0) {
-      return true
+      return true;
     }
-    return false
+    return false;
   };
-​
+
   const onSubmit = async (event) => {
     event.preventDefault();
     if (validateInput(userPayload)) {
@@ -71,8 +72,8 @@ const RegistrationForm = () => {
           if (!response.ok) {
             if (response.status === 422) {
               const body = await response.json();
-              const newServerErrors = translateServerErrors(body.errors)
-              return setServerErrors(newServerErrors)
+              const newServerErrors = translateServerErrors(body.errors);
+              return setServerErrors(newServerErrors);
             }
             const errorMessage = `${response.status} (${response.statusText})`;
             const error = new Error(errorMessage);
@@ -86,22 +87,22 @@ const RegistrationForm = () => {
       }
     }
   };
-​
+
   const onInputChange = (event) => {
     setUserPayload({
       ...userPayload,
       [event.currentTarget.name]: event.currentTarget.value,
     });
   };
-​
+
   if (shouldRedirect) {
     location.href = "/";
   }
-​
+
   return (
     <div className="grid-container">
       <h1>Register</h1>
-	  <ErrorList errors={serverErrors} />
+      <ErrorList errors={serverErrors} />
       <form onSubmit={onSubmit}>
         <div>
           <label>
@@ -141,5 +142,5 @@ const RegistrationForm = () => {
     </div>
   );
 };
-​
-export default RegistrationForm
+
+export default RegistrationForm;
